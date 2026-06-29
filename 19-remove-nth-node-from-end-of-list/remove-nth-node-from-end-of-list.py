59b97c31830e44abcdb0e5ee7b550e4308a1ae88
Vansh Kumar Nigam <151567282+niskarsh7@users.noreply.github.com>
2026-06-29 23:22:00 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (90.66%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -3,20 +3,14 @@ class Solution:
         dummy=ListNode(0)
         dummy.next=head
         curr=head
-        count=0
-        while curr!=None :
-            count+=1
+        jumps=0
+        while jumps< n and curr!=None :
+            jumps+=1
             curr=curr.next
-        curr2=head
+        curr2=curr
         prev=dummy
-        d=count-n+1
-        #0 1 2 3 4 5
-        #      p c 
-        #d=4
-        i=0
-        while i < d-1 :
+        while curr2!=None :
             curr2=curr2.next
             prev=prev.next
-            i+=1
-        prev.next=curr2.next
+        prev.next=prev.next.next
         return dummy.next
