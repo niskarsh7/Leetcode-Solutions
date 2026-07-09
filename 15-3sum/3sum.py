5dfff6aa0995471573464b071f889ba7683fce92
Vansh Kumar Nigam <151567282+niskarsh7@users.noreply.github.com>
2026-07-10 00:56:11 +0530

    Time: 857 ms (21.53%) | Memory: 22.1 MB (81.41%) - LeetSync

---
+++ 15-3sum/3sum.py
@@ -0,0 +1,25 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums.sort()
+        d=[]
+        for i in range(len(nums)):
+            if i >0 and nums[i]==nums[i-1]:
+                continue # it skip duplicates in nums
+            l=i+1
+            r=len(nums)-1
+            while l<r:
+                if nums[l]+nums[r]==-nums[i]:
+                    d.append([nums[i],nums[l],nums[r]])
+                    l+=1
+                    r-=1
+                    while l<r and nums[l]==nums[l-1]:
+                        l+=1
+                    while l<r and nums[r]==nums[r+1]:
+                        r-=1
+                elif nums[l]+nums[r]<-nums[i]:
+                    l+=1
+                else:
+                    r-=1
+        return d 
+
+        
